b6e98cca2598a46945d3c29432e95365a5416196
thinct <thinct_123@foxmail.com>
2023-06-26 21:52:31 +0800

    解决了循环跳转的跟踪表示流程

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     while True:
         eip = dbg.get_register("eip")
         # print(eip,FuncStartIP)
-        # print("eip: {:#X}".format(eip))
-        # print("FuncStartIP: {:#X}".format(FuncStartIP))
+        # print("eip: 0x{:0>8X}".format(eip))
+        # print("FuncStartIP: 0x{:0>8X}".format(FuncStartIP))
         if eip != FuncStartIP:
             dbg.set_debug("run")
             in_key = raw_input("press any key to continue...")
@@ -43,10 +43,21 @@ if __name__ == "__main__":
 
     regsJson = {"AddrFlow":[]}
     DisasmFlow = ""
+    DisasmFlowDirc = {}
+    EIPSet = []
+    EIPGoToSet = []
     while True:
+        dbg.enable_commu_sync_time(False)
         eip = dbg.get_register("eip")
         if eip > FuncEndIP:
             break
+        
+        if eip in EIPSet:
+            dbg.enable_commu_sync_time(True)
+            dbg.set_debug("StepOver")
+            continue
+        EIPSet += [eip]
+         
         eax = dbg.get_register("eax")
         ecx = dbg.get_register("ecx")
         edx = dbg.get_register("edx")
@@ -56,23 +67,30 @@ if __name__ == "__main__":
         esi = dbg.get_register("esi")
         edi = dbg.get_register("edi")
         disasm = dbg.get_disasm_one_code(eip)
-        IPRegs = {"IP":"{:#X}".format(eip), "Disasm":"{}".format(disasm),"Regs":{"eax":"{:#X}".format(eax)\
-        ,"ecx":"{:#X}".format(ecx),"edx":"{:#X}".format(edx),"ebx":"{:#X}".format(ebx),"ebp":"{:#X}".format(ebp)\
-        ,"esp":"{:#X}".format(esp),"esi":"{:#X}".format(esi),"edi":"{:#X}".format(edi)}}
+        IPRegs = {"IP":"'0x{:0>8X}'".format(eip), "Disasm":"{}".format(disasm),"Regs":{"eax":"0x{:0>8X}".format(eax)\
+        ,"ecx":"0x{:0>8X}".format(ecx),"edx":"0x{:0>8X}".format(edx),"ebx":"0x{:0>8X}".format(ebx),"ebp":"0x{:0>8X}".format(ebp)\
+        ,"esp":"0x{:0>8X}".format(esp),"esi":"0x{:0>8X}".format(esi),"edi":"0x{:0>8X}".format(edi)}}
             
-        disasmFlowItem = "/*{:#X}*/    {}".format(eip, disasm)
+        disasmFlowItem = "/*0x{:0>8X}*/    {}".format(eip, disasm)
         if disasm[0] == 'j':
             disasmFlowItem = ";" + disasmFlowItem
+            if len(disasm) == 14:
+                if int(str(disasm[4:14]), 16) < eip:
+                    disasmFlowItem += ";GOTO BACK"
+            if len(disasm) == 13:
+                if int(str(disasm[3:13]), 16) < eip:
+                    disasmFlowItem += ";GOTO BACK"
+                
         elif disasm[0:4] == 'call' and disasm[5:7] == '0x':
-            disasmFlowItem = 'mov eax, ' + disasm[5:15] + '\n' + "/*{:#X}*/    call eax".format(eip)
+            disasmFlowItem = 'mov eax, ' + disasm[5:15] + '\n' + "/*0x{:0>8X}*/    call eax".format(eip)
         #print(IPRegs)
         regsJson["AddrFlow"] += [IPRegs]
+        DisasmFlowDirc[eip] = disasmFlowItem
         DisasmFlow += disasmFlowItem + "\n"
-        print("currentRIP: {:#X} eax: {:#X}".format(eip, eax))
+        print("currentRIP: 0x{:0>8X} eax: 0x{:0>8X}".format(eip, eax))
         
         dbg.enable_commu_sync_time(True)
         dbg.set_debug("StepOver")
-        dbg.enable_commu_sync_time(False)
    
         
     #print(regsJson)
@@ -81,6 +99,12 @@ if __name__ == "__main__":
         
     with open("AddrFlowEasy.asm", "w") as f:
         f.write(DisasmFlow)
+        
+    with open("AddrFlowEasyWithJmp.asm", "w") as f:
+        LsDisasmFlowKeys = list(DisasmFlowDirc.items())
+        LsDisasmFlowKeys.sort(key=lambda x:x[0],reverse=False)
+        for item in LsDisasmFlowKeys:
+            f.write(item[1]+'\n')
      
     dbg.close()
     print("Finished!")
